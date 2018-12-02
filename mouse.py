2c25f9671f73e11d775f16cdf8356c8795b78306
Rawand Hawiz <r.hawiz@geophy.com>
2018-12-02 17:08:40 +0100

    aaaa

--- mouse.py
+++ mouse.py
@@ -1,63 +1,31 @@
-# from time import sleep
-#
-# import uinput
-#
-# events = (
-#     uinput.REL_X,
-#     uinput.REL_Y,
-#     uinput.BTN_LEFT,
-#     uinput.BTN_RIGHT,
-# )
-#
-# with uinput.Device(events) as device:
-#     for x in range(0, 1920):
-#         # syn=False to emit an "atomic" (5, 5) event.
-#         device.emit(uinput.REL_X, x, syn=False)
-#
-#         # Just for demonstration purposes: shows the motion. In real
-#         # application, this is of course unnecessary.
-#         sleep(0.1)
 from time import sleep
 
-import uinput
-
-#
-# def main():
-#     events = (
-#         uinput.REL_X,
-#         uinput.REL_Y,
-#         uinput.BTN_LEFT,
-#         uinput.BTN_RIGHT,
-#     )
-#
-#     with uinput.Device(events) as device:
-#         while True:
-#             # syn=False to emit an "atomic" (5, 5) event.
-#             device.emit(uinput.REL_X, 1, syn=True)
-#
-#             # Just for demonstration purposes: shows the motion. In real
-#             # application, this is of course unnecessary.
-#             sleep(0.25)
-from pymouse import PyMouse
 
 
-def main():
+
+
+def mouse(start_x, end_x):
     m = PyMouse()
-    for i in range(0, 5):
+    for i in range(0, 2):
         print(i)
         sleep(1)
+    c = 0
     while True:
-        for x in range(250, 1571):
+        c += 1
+        for x in range(start_x, end_x, 2):
             m.move(x, 500)
-            sleep(0.3)
+            sleep(0.001)
 
-        m.move(1571, 500)
-        m.release(1571, 500)
-    # for x in range(300, 1200):
-    #     m.move(x, 500)
-    #
-    #     sleep(0.25)
+        # m.press(start_x, 500)
+        m.move(start_x, 500)
+        m.drag(end_x, 500)
+        if c == 2:
+            break
 
 
+
+
+
+#
 if __name__ == "__main__":
-    main()
+    mover(1800)
--- tradingview.py
+++ tradingview.py
@@ -1,18 +1,29 @@
+import base64
 import csv
+import datetime
+import io
 import os
+import re
+import time
 from threading import Thread
-from time import sleep, time
+from time import sleep
 
 import logging
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
+from pymouse import PyMouse
+from pynput.keyboard import Key, Controller
+
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium import webdriver
-import sqlite3
+from selenium.webdriver.common.by import By
+
+try:
+    from PIL import Image, ImageFilter
+except ImportError:
+    import Image
+
+import pytesseract
 
 base = os.path.dirname(os.path.realpath(__file__))
 load_dotenv(os.path.join(base, '.env'))
@@ -37,21 +48,15 @@ driver = webdriver.Chrome(os.path.join(base, DRIVER_PATH))
 
 driver.maximize_window()
 
-driver.get("https://uk.tradingview.com/#signin")
-
-username = WebDriverWait(driver, 20).until(expected_conditions.presence_of_element_located((By.NAME, 'username')))
-password = WebDriverWait(driver, 20).until(expected_conditions.presence_of_element_located((By.NAME, 'password')))
-
-username.send_keys(USERNAME)
-password.send_keys(PASSWORD)
-
-login = WebDriverWait(driver, 20).until(expected_conditions.presence_of_element_located((By.XPATH,
-                                                                                         "//button[@type='submit']")))
-
-login.submit()
 # input("anykey to continue")
 
+huOpenSelector = 'return document.querySelectorAll("body > div.js-rootresizer__contents > div.layout__area--center > div > div.chart-container-border > div.chart-widget > table > tbody > tr:nth-child(1) > td.chart-markup-table.pane > div > div.pane-legend > div.pane-legend-line.pane-legend-wrap.main > div > span:nth-child(1) > span.pane-legend-item-value.pane-legend-line.pane-legend-item-value__main")[0].textContent'
+huHighSelector = 'return document.querySelectorAll("body > div.js-rootresizer__contents > div.layout__area--center > div > div.chart-container-border > div.chart-widget > table > tbody > tr:nth-child(1) > td.chart-markup-table.pane > div > div.pane-legend > div.pane-legend-line.pane-legend-wrap.main > div > span:nth-child(2) > span.pane-legend-item-value.pane-legend-line.pane-legend-item-value__main")[0].textContent'
+huLowSelector = 'return document.querySelectorAll("body > div.js-rootresizer__contents > div.layout__area--center > div > div.chart-container-border > div.chart-widget > table > tbody > tr:nth-child(1) > td.chart-markup-table.pane > div > div.pane-legend > div.pane-legend-line.pane-legend-wrap.main > div > span:nth-child(3) > span.pane-legend-item-value.pane-legend-line.pane-legend-item-value__main")[0].textContent'
+huCloseSelector = 'return document.querySelectorAll("body > div.js-rootresizer__contents > div.layout__area--center > div > div.chart-container-border > div.chart-widget > table > tbody > tr:nth-child(1) > td.chart-markup-table.pane > div > div.pane-legend > div.pane-legend-line.pane-legend-wrap.main > div > span:nth-child(4) > span.pane-legend-item-value.pane-legend-line.pane-legend-item-value__main")[0].textContent'
 
+huVol1Selector = 'return document.querySelectorAll("body > div.js-rootresizer__contents > div.layout__area--center > div > div.chart-container-border > div.chart-widget > table > tbody > tr:nth-child(1) > td.chart-markup-table.pane > div > div.pane-legend > div.pane-legend-line.pane-legend-wrap.study > div > span:nth-child(1) > span")[0].textContent'
+huVol2Selector = 'return document.querySelectorAll("body > div.js-rootresizer__contents > div.layout__area--center > div > div.chart-container-border > div.chart-widget > table > tbody > tr:nth-child(1) > td.chart-markup-table.pane > div > div.pane-legend > div.pane-legend-line.pane-legend-wrap.study > div > span:nth-child(2) > span")[0].textContent'
 
 done = set()
 
@@ -63,35 +68,132 @@ if os.path.isfile("data.csv"):
             done.add(row[0])
 
 
-def main():
-    with open("data.csv", "a") as f:
+def mover(start_x):
+    sleep(4)
+    m = PyMouse()
+    m.move(start_x, 500)
+    keyboard = Controller()
+
+    while True:
+        keyboard.press(Key.left)
+        sleep(100)
+
+
+def parse_time(text):
+    text = text.split("\n")
+    if len(text) == 2:
+        text = text[1]
+    else:
+        text = text[0]
+
+    text = text.replace("'", "").replace("`", "").replace("-", "").replace("‘", "")
+    text = re.sub("\\s+", " ", text)
+    return time.mktime(datetime.datetime.strptime(text, "%d %b %y %H:%M").timetuple())
+
+
+def extract_text(b64_encoded):
+    image = Image.open(io.BytesIO(base64.b64decode(b64_encoded)))
+    image = image.crop(image.getbbox())
+    image = image.filter(ImageFilter.SHARPEN)
+    image = image.filter(ImageFilter.DETAIL)
+    width = 300
+    wpercent = (width / float(image.size[0]))
+    hsize = int((float(image.size[1]) * float(wpercent)))
+    image = image.resize((width, hsize), Image.ANTIALIAS)
+    return pytesseract.image_to_string(image)
+
+
+def collect():
+    keyboard = Controller()
+    with open("{}_{}.csv".format(EXCHANGE, SYMBOL), "a") as f:
         writer = csv.writer(f)
 
         while True:
-            t0 = time()
+            # for i, canvas in enumerate(driver.find_elements_by_tag_name("canvas")):
+
             try:
-                huDate = driver.find_element_by_id('huDate').text
-                huDate = huDate.replace("-", "").replace(":", "").replace(" ", "")
-                huOpen = driver.find_element_by_id('huOpen').text
-                huHigh = driver.find_element_by_id('huHigh').text
-                huLow = driver.find_element_by_id('huLow').text
-                huClose = driver.find_element_by_id('huClose').text
-                huVolume = driver.find_element_by_id('huVolume').text
+                canvas = driver.find_elements_by_tag_name("canvas")[8]
+                canvas_base64 = driver.execute_script("return arguments[0].toDataURL('image/png').substring(21);",
+                                                      canvas)
+                time_text = extract_text(canvas_base64)
+                timestamp = int(parse_time(time_text))
+                huOpen = driver.execute_script(huOpenSelector)
+                huHigh = driver.execute_script(huHighSelector)
+                huLow = driver.execute_script(huLowSelector)
+                huClose = driver.execute_script(huCloseSelector)
+                huVolume1 = driver.execute_script(huVol1Selector)
+                huVolume2 = driver.execute_script(huVol2Selector)
+            except Exception as e:
+                print(e)
+                continue
 
-                if huDate not in done:
-                    writer.writerow([
-                        huDate,
-                        huOpen,
-                        huHigh,
-                        huLow,
-                        huClose,
-                        huVolume
-                    ])
-                    done.add(huDate)
+            if timestamp not in done:
+                writer.writerow([
+                    timestamp,
+                    huOpen,
+                    huHigh,
+                    huLow,
+                    huClose,
+                    huVolume1,
+                    huVolume2,
+                ])
+                done.add(timestamp)
 
-                print(len(done), huDate)
-            except Exception:
-                pass
+                print(len(done), timestamp, huOpen, huHigh, huLow, huClose, huVolume1, huVolume2)
+            keyboard.press(Key.left)
+            keyboard.release(Key.left)
 
 
-main()
+if __name__ == '__main__':
+    EXCHANGE = "NASDAQ"
+    SYMBOL = "GOOG"
+    driver.get("https://uk.tradingview.com/#signin")
+    print(">> Loading home page")
+
+    username = WebDriverWait(driver, 20).until(expected_conditions.presence_of_element_located((By.NAME, 'username')))
+    password = WebDriverWait(driver, 20).until(expected_conditions.presence_of_element_located((By.NAME, 'password')))
+
+    username.send_keys(USERNAME)
+    password.send_keys(PASSWORD)
+    print(">> Logging in")
+
+    login = WebDriverWait(driver, 20).until(expected_conditions.presence_of_element_located((By.XPATH,
+                                                                                             "//button[@type='submit']")))
+
+    login.submit()
+    print(">> Logged in")
+
+    print(">> Loading Stock Page")
+    driver.get("https://uk.tradingview.com/chart?symbol={}:{}".format(EXCHANGE, SYMBOL))
+
+    sleep(2)
+    driver.execute_script(
+        'document.querySelectorAll("body > div.js-rootresizer__contents > div.layout__area--center > div > div.chart-container-border > div.chart-controls-bar > div > div:nth-child(2) > div > div.sliderRow-Tv1W7hM5-.tabs-1LGqoVz6- > div.item-3cgIlGYO-.isFirst-2kfAV5tf- > div.apply-common-tooltip")[0].click()'
+    )
+    sleep(1)
+    driver.execute_script(
+        'document.querySelectorAll("body > div.js-rootresizer__contents > div.layout__area--center > div > div.chart-container-border > div.chart-widget > table > tbody > tr:nth-child(1) > td.chart-markup-table.pane > div > div.pane-legend > div.pane-legend-line.pane-legend-wrap.study > span.pane-legend-icon-container > a.pane-legend-icon.apply-common-tooltip.format")[0].click()'
+    )
+    sleep(1)
+    driver.execute_script(
+        'document.querySelectorAll("body > div._tv-dialog._tv-dialog-nonmodal.ui-draggable > div._tv-dialog-content > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(2) > input")[0].value = 1'
+    )
+    sleep(2)
+    driver.execute_script(
+        'return document.querySelectorAll("body > div._tv-dialog._tv-dialog-nonmodal.ui-draggable > div._tv-dialog-content > div.main-properties.main-properties-aftertabs > div > a._tv-button.ok")[0].click()')
+    print(">> Collecting Data")
+    sleep(2)
+
+    start_coords = driver.find_element_by_class_name("icon-2Gun4jqH-").location
+
+    print(start_coords)
+    m = PyMouse()
+    m.move(start_coords["x"] - 100, 500)
+    # mover_thread = Thread(target=mover, args=(start_coords["x"] - 100,))
+    # mover_thread.start()
+
+    input("Anykey to start")
+    for i in range(0, 5):
+        print("Starting in {}....".format(5 - i))
+        sleep(1)
+    collect()
